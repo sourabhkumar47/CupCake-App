90817e1c3b9abcb2b9d01237a32aaa4066044c0e
Sourabh Kumar <20010001030@gateway.edu.in>
2022-06-04 23:31:48 +0530

    Reference to shared ViewModel

--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -7,8 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentStartBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
  * This is the first screen of the Cupcake app. The user can choose how many cupcakes to order.
@@ -19,6 +21,9 @@ class StartFragment : Fragment() {
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
     // when the view hierarchy is attached to the fragment.
     private var binding: FragmentStartBinding? = null
+
+    //get a reference to shared ViewModel
+    private val sharedViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -1,4 +1,37 @@
 package com.example.cupcake.model
 
-class OrderViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+class OrderViewModel : ViewModel() {
+
+    //Added Properties
+    //add backing fields
+    private val _quantity = MutableLiveData<Int>(0)
+    val quantity: LiveData<Int> = _quantity
+
+    private val _flavor = MutableLiveData<String>("")
+    val flavor: LiveData<String> = _flavor
+
+    private val _date = MutableLiveData<String>("")
+    val date: LiveData<String> = _date
+
+    private val _price = MutableLiveData<Double>(0.0)
+    val price: LiveData<Double> = _price
+
+    //update the properties above, depending on the user's choice:
+    fun setQuantity(numberCupCakes: Int) {
+        _quantity.value = numberCupCakes
+    }
+
+    fun setFlavour(desiredFlavour: String) {
+        _flavor.value = desiredFlavour
+    }
+
+    fun setDate(pickupDate: String) {
+        _date.value = pickupDate
+    }
+
 }
